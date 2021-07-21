ee1486c01b70ee95ad5db00a3947ec34c3fb915b
gdefa <85749259+gdefa@users.noreply.github.com>
2021-07-21 14:38:40 +0200

    déclaration variable Js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Les varibales //
+
+let newGame = document.getElementById('newGameButton');
+var joueur1 = document.getElementById('player1');
+var joueur2 = document.getElementById('player2');
+var resultatGlobalJ1 = document.getElementById('globalScore1');
+var resultatGlobalJ2 = document.getElementById ('gobaleScore2');
+var currentScoreJ1 = document.getElementById('currentScoreJ1');
+var currentScoreJ2 = document.getElementById('currentScoreJ2');
+
+const buttonRollDice = document.getElementsByClassName('RollDiceButton');
+const buttonHoldDice = document.getElementsByClassName('holdDiceButton');
