54d76384a7982cb104332c5a99903259b06ab4ae
gdefa <85749259+gdefa@users.noreply.github.com>
2021-07-21 15:58:17 +0200

    Mise en place function init, new game

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 // Les varibales //
 
-let newGame = document.getElementById('newGameButton');
+
 var joueur1 = document.getElementById('player1');
 var joueur2 = document.getElementById('player2');
-var resultatGlobalJ1 = document.getElementById('globalScore1');
-var resultatGlobalJ2 = document.getElementById ('gobaleScore2');
+var scoreGlobalJ1 = document.getElementById('scoreGlobalJ1');
+var scoreGlobalJ2 = document.getElementById ('scoreGlobalJ2');
 var currentScoreJ1 = document.getElementById('currentScoreJ1');
 var currentScoreJ2 = document.getElementById('currentScoreJ2');
 
 const buttonRollDice = document.getElementsByClassName('RollDiceButton');
 const buttonHoldDice = document.getElementsByClassName('holdDiceButton');
+
+// initialisation nouvelle partie 
+
+document.getElementById('newGameInit').addEventListener("click", function(){
+    alert("Vous lancez une nouvelle partie !");
+    init()
+});
+
+function init(){
+    if (newGameInit === true){
+        scoreGlobalJ1 = 0;
+        scoreGlobalJ2 = 0;
+    }
+    
+}
+
+
+    
