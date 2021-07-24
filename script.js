cef8451be74a08aa9aa60cfba9978e81b3b55cb6
gdefa <85749259+gdefa@users.noreply.github.com>
2021-07-24 14:22:12 +0200

    Ajout var img + newgame function

--- script.js
+++ script.js
@@ -8,23 +8,29 @@ var scoreGlobalJ2 = document.getElementById ('scoreGlobalJ2');
 var currentScoreJ1 = document.getElementById('currentScoreJ1');
 var currentScoreJ2 = document.getElementById('currentScoreJ2');
 
-const buttonRollDice = document.getElementsByClassName('RollDiceButton');
-const buttonHoldDice = document.getElementsByClassName('holdDiceButton');
+
+const buttonHoldDice = document.getElementByClassName('holdDiceButton');
+
+var imgDiceRandom = [
+    "images/des_1.jpg",
+    "images/des_2.jpg",
+    "images/des_3.jpg",
+    "images/des_4.jpg",
+    "images/des_5.jpg",
+    "images/des_6.jpg",
+];
 
 // initialisation nouvelle partie 
 
-document.getElementById('newGameInit').addEventListener("click", function(){
-    alert("Vous lancez une nouvelle partie !");
-    init()
-});
+document.getElementById("newGameInit").onclick= init;
 
 function init(){
-    if (newGameInit === true){
-        scoreGlobalJ1 = 0;
-        scoreGlobalJ2 = 0;
-    }
-    
-}
+
+    scoreGlobalJ1.textContent=0;
+    scoreGlobalJ2.textContent=0;
+    currentScoreJ1.textContent=0;
+    currentScoreJ2.textContent=0;
+
+};
 
 
-    
